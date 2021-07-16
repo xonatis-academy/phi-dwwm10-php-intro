b71aec69809435c8171adac2e2d8795c5dd6be3e
xonatis-academy <formation@xonatis.com>
2021-07-16 17:03:07 +0200

    Adding exercises

--- exercices/corrige.php
+++ exercices/corrige.php
@@ -51,3 +51,108 @@ function faireSomme(array $tab): float
     }
     return $somme;
 }
+
+/*
+
+### 8. Trouver l'élément maximum d'un tableau
+
+Problème : Ecrire une fonction qui prend un tableau en paramètre et retourne l'élément maximum
+la plus grande valeur et en particulier, vous devez utiliser une boucle
+
+*/
+
+// 1. On choisit le nom de la fonction (verbe) : trouverMaximum
+// 2. On déduit le type des paramètres en entrée : array $tab
+// 3. On déduit le type de la valeur de retour : float
+
+function trouverMaximum(array $tab): float
+{
+    // **METTRE** le doigt sur le 1er element de la liste
+    $doigt = $tab[0];
+
+    // **POUR CHAQUE** element DANS la liste
+    for ($i = 1; $i < count($tab); ++$i)
+    {
+        $element = $tab[$i];
+        
+        // **SI** l'element est plus grand que le doigt
+        if ($element > $doigt)
+        {
+            // **METTRE** le doigt sur l'element
+            $doigt = $element;
+        }
+    }
+
+    // **RETOURNER** le doigt
+    return $doigt;
+}
+
+$top = [34.5, 4.3, 45.5];
+$res = trouverMaximum($top);
+var_dump($res);
+
+/*
+
+### 9. Trouver l'élément minimum d'un tableau
+
+Problème : Ecrire une fonction qui prend un tableau en paramètre et retourne l'élément minimum
+(le plus petit element du tableau)
+le tableau est un tableau de nombres flottants
+
+*/
+
+// 1. On choisit le nom de la fonction (verbe) : trouverMinimum
+// 2. On déduit le type des paramètres en entrée : array $tab
+// 3. On déduit le type de la valeur de retour : float
+
+function trouverMinimum(array $tab): float
+{
+    // **METTRE** le doigt sur le premier element
+    $doigt = $tab[0];
+    // **POUR CHAQUE** element DANS $tab
+    for ($i = 0; $i < count($tab); ++$i)
+    {
+        $element = $tab[$i];
+        // **SI** l'element est plus petit que le doigt
+        if ($element < $doigt)
+        {
+            // **METTRE** le doigt sur l'elemenet
+            $doigt = $element;
+        }
+    }
+    // **RETOURNE** le doigt
+    return $doigt;
+}
+
+
+/*
+
+### 10. Trouver le premier élément supérieur à un nombre
+
+Problème : Ecrire une fonction qui prend un tableau et un nombre floattant en paramètre 
+et retourne le premier élément supérieur à ce nombre s'il y en a, sinon null. 
+Vous pourrez utiliser le type ?float à la place du type float
+
+*/
+
+// 1. On choisit le nom de la fonction (verbe) : trouverPremier
+// 2. On déduit le type des paramètres en entrée : array $tab, float $reference
+// 3. On déduit le type de la valeur de retour : ?float
+
+function trouverPremier(array $tab, float $reference): ?float
+{
+    // **POUR CHAQUE** element DANS $tab
+    for ($i = 0; $i < count($tab); ++$i)
+    {
+        $element = $tab[$i];
+        // **SI** element > $reference
+        if ($element > $reference)
+        {
+            // **RETOURNER** l'element
+            return $element;
+        }
+    }
+    // **RETOURNER** null
+    return null;
+}
+
